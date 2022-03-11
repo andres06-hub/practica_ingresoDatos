8c24a88d284f1dff2b59360383563f9bee8929ad
andres06 <andressierrarojas7@gmail.com>
2022-03-11 11:32:40 -0500

    se obtiene los datos y crea el modelo map

--- app/src/main/java/com/simplelogin/MainActivity.kt
+++ app/src/main/java/com/simplelogin/MainActivity.kt
@@ -15,18 +15,33 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun model(){
-        var mapDatos = mutableMapOf<String, String>();
-    }
+
 
     private fun onClick(){
+
+
+
+
+    }
+
+    private fun obtenerDatos(){
+
+        //obtenemos el modelo donde guardamos los datos
+        var datos = model();
+
+
+
         //Obtenemos los valores
         var documet : EditText = findViewById(R.id.txt_docum)
         var name : EditText = findViewById(R.id.txt_name)
         var email : EditText = findViewById(R.id.txt_email)
         var password : EditText = findViewById(R.id.txt_pass)
+    }
 
-
+    // Funcion que retorna el modelo
+    private fun model() : MutableMap<String, String> {
+        var mapDatos = mutableMapOf<String, String>();
+        return mapDatos;
     }
 
 
